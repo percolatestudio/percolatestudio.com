62581b2ad3a5d0ac4988bad112bcc3ff7580af6b
Dominic <dom@percolatestudio.com>
2014-08-05 15:29:32 -0700

    Content for the pages and stylesheets

--- client/application/layout.js
+++ client/application/layout.js
@@ -1,14 +1,21 @@
 var CONTACT_KEY = 'contactOverlayOpen';
+var MENU_KEY = 'menuOverlayOpen';
 
 Session.setDefault(CONTACT_KEY, false);
 Template.layout.helpers({
   contactOpenClass: function() {
     return Session.equals(CONTACT_KEY, true) && 'contact-open';
+  },
+  menuOpenClass: function() {
+    return Session.equals(MENU_KEY, true) && 'menu-open';
   }
 });
 
 Template.layout.events({
   'click [data-contact]': function() {
     Session.set(CONTACT_KEY, true);
+  },
+  'click [data-menu]': function() {
+    Session.set(MENU_KEY, true);
   }
 });

d7dae35d4d4180c0eba21753d8103c7a047d47ab
Tom Coleman <tom@thesnail.org>
2014-08-08 09:19:47 +1000

    Added a package version

--- packages/static-collections/package.js
+++ packages/static-collections/package.js
@@ -1,5 +1,6 @@
 Package.describe({
-  summary: "Static data collections from markdown"
+  summary: "Static data collections from markdown",
+  version: "0.0.0"
 });
 
 Package._transitional_registerBuildPlugin({

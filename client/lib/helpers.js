2117eecc9448c9c09caebf7b92ecddd9d65e66b8
Tom Coleman <tom@thesnail.org>
2015-01-23 10:54:01 +1100

    Added retina image links.

---
+++ client/lib/helpers.js
@@ -0,0 +1,6 @@
+Template.registerHelper('imgSrc', function(original) {
+  if (! window.devicePixelRatio || window.devicePixelRatio <= 1)
+    return original;
+  
+  return original.replace(/\.[^\.]*$/, '@2x$&');
+});

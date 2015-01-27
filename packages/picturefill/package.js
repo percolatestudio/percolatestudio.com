7e0ed12b44a6fd22612005e5e0c735c767b04584
Tom Coleman <tom@thesnail.org>
2015-01-27 15:52:04 +1100

    Added picturefill package. Seems to work.

---
+++ packages/picturefill/package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'picturefill',
+  summary: 'Picturefill polyfill for HTML5 picture elements',
+  version: '2.2.0',
+  git: ' /* Fill me in! */ '
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.use('iron:router');
+  api.addFiles(['picturefill-2.2.0.js', 'picturefill.js'], 'client');
+});

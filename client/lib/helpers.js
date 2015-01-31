dae0844fd63f9f3d0ed72331329f55f86365c3f7
Tom Coleman <tom@thesnail.org>
2015-01-31 12:56:41 +1100

    Switch featured product image depending on screen size

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,6 +1,18 @@
-Template.registerHelper('imgSrc', function(original) {
+var RESPONSIVE_BREAKPOINT = 800;
+
+var imgSrc = function(original) {
   if (! window.devicePixelRatio || window.devicePixelRatio <= 1)
     return original;
   
   return original.replace(/\.[^\.]*$/, '@2x$&');
+};
+
+Template.registerHelper('imgSrc', imgSrc);
+
+Template.registerHelper('responsiveImgSrc', function(smallSrc, largeSrc) {
+  if (Measurement.getWindowSize().width <= RESPONSIVE_BREAKPOINT) {
+    return imgSrc(smallSrc);
+  } else {
+    return imgSrc(largeSrc);
+  }
 });

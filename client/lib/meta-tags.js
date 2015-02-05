690d1d4a70f4c6895a7814f8fda576ce6a084d62
Tom Coleman <tom@thesnail.org>
2015-02-06 10:04:27 +1100

    Fix title and description meta

--- client/lib/meta-tags.js
+++ client/lib/meta-tags.js
@@ -1,9 +1,9 @@
-UI.registerHelper('metaTitle', new Template('title', function () {
-  var text = this.templateContentBlock.renderFunction();
+UI.registerHelper('metaTitle', new Template('metaTitle', function () {
+  var text = Blaze.toHTML(this.templateContentBlock, this);
   $('head > title').text(text);
 }));
 
-UI.registerHelper('metaDescription', new Template('description', function () {
-  var text = this.templateContentBlock.renderFunction();
+UI.registerHelper('metaDescription', new Template('metaDescription', function () {
+  var text = Blaze.toHTML(this.templateContentBlock, this);
   $('head > meta[name=description]').attr('content', text);
 }));

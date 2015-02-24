d2ba078d26c61b4619affbc1d64e2d1378f6b2c1
Tom Coleman <tom@thesnail.org>
2015-02-24 17:53:29 +1100

    Added job

--- client/templates/job.jsx
+++ client/templates/job.jsx
@@ -1,5 +1,24 @@
 Job = React.createClass({
+  mixins: [MetaTagsMixin],
+  
+  job: function() {
+    var name = this.props.params.name;
+    return this.props.collections.Jobs.findOne({name: name}) ||
+      this.props.collections.Interns.findOne({name: name});
+  },
+  
+  componentWillMount: function() {
+    var job = this.job();
+    this.setTitle(job.title + '| Percolate Studio');
+    this.setDescription(job.description);
+  },
+  
   render: function() {
-    return <div/>;
+    var job = this.job();
+    return (
+      <PageLayout className="job-{job.name}">
+        <div dangerouslySetInnerHTML={{__html: job.text}} />
+      </PageLayout>
+    )
   }
 });

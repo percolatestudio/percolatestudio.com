7c7de2463231b1c846ff2fd52772f059a246393f
Tom Coleman <tom@thesnail.org>
2015-02-25 12:19:29 +1100

    Achieved contact overlay route.

--- client/templates/application/layout.jsx
+++ client/templates/application/layout.jsx
@@ -2,7 +2,14 @@ Layout = React.createClass({
   getInitialState: function() {
     return {
       menuOpen: false,
+      menuOverlaid: false,
       contactOpen: false
+    }
+  },
+  
+  componentWillMount: function() {
+    if (location.hash === '#contact') {
+      this.setState({contactOpen: true});
     }
   },
   
@@ -21,6 +28,7 @@ Layout = React.createClass({
   
   openContact: function(state) {
     this.setState({contactOpen: state});
+    location.hash = state ? 'contact' : '';
   },
   
   handleKeyDown: function(event) {
@@ -56,3 +64,54 @@ Layout = React.createClass({
     )
   }
 });
+
+
+  // var OVERLAY_OFFSET = 100;
+  // // XXX: should we find a requestAnimationFrame version of _.throttle?
+  // self.$('.page').scroll(_.throttle(function() {
+  //   var offset = self.find('.page').scrollTop;
+  //
+  //   if (offset > OVERLAY_OFFSET) {
+  //     self.menuOverlaid.set(offset >= lastOffset ? 'down' : 'up');
+  //   } else {
+  //     self.menuOverlaid.set(false);
+  //   }
+  //
+  //   lastOffset = offset;
+  // }, 100));
+  //
+  // // Wire up the contact form "route". Perhaps it's better to do it
+  // //   with IR as in atmosphere but this is a pretty simple approach.
+  // self.autorun(function() {
+  //   self.contactOpen.set(Iron.Location.get().hash === '#contact');
+  // });
+  //
+  // self.autorun(function() {
+  //   location.hash = self.contactOpen.get() ? 'contact' : '';
+  // });
+  //
+  // var SCROLLABLE = '.page';
+  //
+  // var setHistoryState = function (ourState) {
+  //   var state = Iron.Location.get().options.historyState;
+  //   var newState  = _.extend({}, state, ourState);
+  //   Iron.Location.go(Iron.Location.get().path, {historyState: newState, replaceState: true});
+  // }
+  //
+  // Template.pageLayout.rendered = function() {
+  //   var self = this;
+  //
+  //   var state = Iron.Location.get().options.historyState;
+  //   if (state && state.lastScrollTop) {
+  //     // wait until it's on the screen (not sure why afterFlush doesn't work)
+  //     Meteor.setTimeout(function() {
+  //       self.$(SCROLLABLE).scrollTop(state.lastScrollTop);
+  //     }, 0);
+  //   }
+  //
+  //   // XXX: should we find a requestAnimationFrame version of _.throttle?
+  //   self.$(SCROLLABLE).scroll(_.throttle(function() {
+  //     var offset = self.find(SCROLLABLE).scrollTop;
+  //     setHistoryState({lastScrollTop: offset})
+  //   }, 500));
+  // }

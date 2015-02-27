cf26e2fe449594ec168f2fd132d69f99e58b263e
Tom Coleman <tom@thesnail.org>
2015-02-27 12:39:51 +1100

    Whoops! Fixed up not found.

--- client/templates/not-found.jsx
+++ client/templates/not-found.jsx
@@ -1,5 +1,16 @@
 NotFound = React.createClass({
   render: function() {
-    return <div/>;
+    return (
+      <PageLayout {...this.props}>
+        <div className="header">
+          <div className="wrapper-hero">
+            <img title="404" className="hero" src="/img/notfound-160x160.svg"/>
+            <h1 className="title-page">Not Found</h1>
+            <p className="description-page">Yikes! The content you were looking for cannot be found.</p>
+            <a className="btn-secondary caps" href="/">Return Home</a>
+          </div>
+        </div>
+      </PageLayout>
+    );
   }
 });

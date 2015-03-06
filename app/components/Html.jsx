c0128ef1821b8d905f3146666b09005a9714f811
Zoltan Olah <zol@percolatestudio.com>
2015-03-05 16:55:52 -0800

    I think this is hooked up but I’m not sure that localhost is a valid test #38

--- app/components/Html.jsx
+++ app/components/Html.jsx
@@ -2,14 +2,26 @@
 var React = require('react');
 var HeadParams = require('../lib/HeadParams');
 
+// configure these
+var GA_ACCOUNT = 'UA-32959034-1';
+var GA_DOMAIN = 'percolatestudio.com';
+
+// From: https://developers.google.com/analytics/devguides/collection/gajs/
+var gaSnippet = "var _gaq = _gaq || []; \
+_gaq.push(['_setAccount', ' " + GA_ACCOUNT + "']); \
+_gaq.push(['_trackPageview']); \
+_gaq.push(['_setDomainName', '" + GA_DOMAIN + "']); \
+(function() { \
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; \
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; \
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); \
+})();";
+
 var Html = React.createClass({
   propTypes: {
     headParams: React.PropTypes.instanceOf(HeadParams)
   },
   render: function() {
-    // FIXME: do this properly & test the async snippet https://developers.google.com/analytics/devguides/collection/gajs/
-    var gaScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-32959034-1', 'percolatestudio.com');";
-    
     return (
       <html>
         <head>
@@ -24,7 +36,7 @@ var Html = React.createClass({
           <link rel="shortcut icon" type="image/png" href="/favicon.png" sizes="16x16 32x32 64x64" />
           <link href="/client.css" media="all" rel="stylesheet" />
 
-          <script dangerouslySetInnerHTML={{__html: gaScript}}></script>
+          <script dangerouslySetInnerHTML={{__html: gaSnippet}}></script>
         </head>
         <body dangerouslySetInnerHTML={{__html: this.props.markup}} />
         <script src="/client.js" async></script>
